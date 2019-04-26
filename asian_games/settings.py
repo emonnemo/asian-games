6e102c2ba7c4ea3c9d192876bde3e61be31fa302
andika <dika.kusuma23@gmail.com>
2019-04-27 01:17:26 +0700

    Fix settings for static files

--- asian_games/settings.py
+++ asian_games/settings.py
@@ -123,4 +123,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'root-static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
 
